a96fb0da300ac144ce23c43b89b31e090023dd53
Vishesh2445 <visheshsingh7655@gmail.com>
2025-08-03 12:36:59 +0530

    Create firebaseService.js

---
+++ firebaseService.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+module.exports = db;
